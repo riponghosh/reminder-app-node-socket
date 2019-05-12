07f24471808bb6668b7c94ebbc2319331d7a7bea
Ripon Ghosh <ripon.ghosh200@gmail.com>
2019-05-12 14:49:37 +0600

    node schedule run

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+var schedule = require('node-schedule');
+const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) =>{
+	res.send('Hello World!');
+	var date = new Date('2019-05-12 14:18:55.140');
+	var j = schedule.scheduleJob(date, function(y){
+		console.log('/ url')
+		console.log(y);
+	});
+})
+for(let i=0;i<5;i++){
+	let date = new Date('2019-05-12 14:44:13.140');
+	let j = schedule.scheduleJob(date, function(y){
+		console.log('loop')
+		console.log(y);
+	});
+}
+console.log('run')
+var date = new Date('2019-05-12 14:19:13.140');
+var j = schedule.scheduleJob(date, function(y){
+	console.log('home')
+	console.log(y);
+});
 
+
+setTimeout((arg)=>console.log(`arg was => ${arg}`), 10000, 'funky');
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
